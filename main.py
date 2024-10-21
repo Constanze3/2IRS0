2b643d45bfc039d06803a27f57d2a6ad4455c7d1
Matheus Tran <matheus@gmail.com>
2024-10-21 12:05:16 +0200

    function to generate your own graphs

--- main.py
+++ main.py
@@ -6,20 +6,17 @@ from pathfinding import Pathfinder
 
 from baruah import build_routing_tables
 
-def plot_graph(node_count, ax):
+def generate_random_graph(node_count, overall_max_time=20):
     edge_count = random.randint(node_count - 1, int((node_count * node_count - 1) / 2)) 
-    overall_max_time = 20
 
     G = nx.Graph()
     G.add_nodes_from([0, node_count - 1])
-    edge_labels = {}
 
     def create_edge(u, v):
         typical_time = random.randint(1, overall_max_time)
         max_time = random.randint(typical_time, overall_max_time)
 
         G.add_edge(u, v, typical_delay=typical_time, max_delay=max_time)
-        edge_labels[(u, v)] = (typical_time, max_time)
 
     possible_edges = [(i, j) for i in range(node_count) for j in range(node_count) if i != j]
 
@@ -37,36 +34,36 @@ def plot_graph(node_count, ax):
         index = random.randrange(len(possible_edges))
         edge = possible_edges.pop(index)
         create_edge(*edge)
+    return G
 
-    # consider only the typical delay for pathfinding (index 0 of edge attributes)
-    G_for_pathfinding = {node: {key: list(value.values())[0] for key, value in edge.items()} for node, edge in G.adjacency()}
+test_graph = [
+    [None, (6, 10), None, None, (30, 50)],
+    [(6, 10), None, (2, 5), (16, 20), (20, 30)],
+    [None, (2, 5), None, (18, 19), (15, 20)],
+    [None, (16, 20), (18, 19), None, (12, 15)],
+    [(30, 50), (20, 30), (15, 20), (12, 15)]
+]
 
-    pathfinder = Pathfinder(G_for_pathfinding, 0)
-    path = pathfinder.find_path(node_count - 1)
+def from_adjacency_matrix(matrix):
+    G = nx.Graph()
+    G.add_nodes_from([0, len(matrix) - 1])
+    for u, nodes in enumerate(matrix):
+        for v, delays in enumerate(nodes[u:], start=u):
+            if delays:
+                print((u, v))
+                G.add_edge(u, v, typical_delay=delays[0], max_delay=delays[1])
+    return G
 
-    print(f"shortest path from {0} to {node_count - 1}")
-    print(path)
 
-    path_edges = []
-    for i in range(len(path) - 1):
-        path_edges.append((path[i], path[i + 1]))
-
+def plot_graph(G, ax, pos):
     colors = ["b"] * len(G.edges())
-    
-    pos = nx.spring_layout(G)
     nx.draw_networkx(G, pos, with_labels=True, ax=ax, edge_color=colors)
     nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G, "typical_delay"), ax=ax, label_pos=0.3)
     nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G, "max_delay"), ax=ax, font_color='red', label_pos=0.7)
 
-    # baruah
-    # Node = Any
-    # # maps a node to a tuple of (typical delay, max delay)
-    # Edges = Mapping[Node, (float, float)]
-    # Graph = Mapping[Node, Edges]
-
-    # shut up
+def baruah(G, ax, pos):
     G_for_baruah = {node: {key: (list(value.values())[0], list(value.values())[1]) for key, value in edge.items()} for node, edge in G.adjacency()}
-    table = build_routing_tables(G_for_baruah, node_count - 1)
+    table = build_routing_tables(G_for_baruah, len(G_for_baruah.keys()) - 1)
 
     def format_node(node_tuples):
         result = ""
@@ -74,44 +71,50 @@ def plot_graph(node_count, ax):
             result += f"{node_tuple}\n"
         result += "\n"
         return result
-
-
     baruah_labels = {node: format_node(table[node]) for node in table}
     nx.draw_networkx_labels(G, pos, labels=baruah_labels, ax=ax)
 
-node_count = 5
+if __name__ == "__main__":
+    node_count = 5
+    fig, ax = plt.subplots()
+    plt.subplots_adjust(bottom=0.2)
 
-fig, ax = plt.subplots()
-plt.subplots_adjust(bottom=0.2)
+    # graph = generate_random_graph(node_count)
+    graph = from_adjacency_matrix(test_graph)
+    pos = nx.spring_layout(graph)
+    plot_graph(graph, ax, pos)
+    baruah(graph, ax, pos)
 
-plot_graph(node_count, ax)
-
-def refresh(event):
-    ax.clear()
-    plot_graph(node_count, ax)
-    fig.canvas.draw()
-
-axbutton = plt.axes((0.2, 0.05, 0.15, 0.07))
-button = Button(axbutton, "regenerate")
-button.on_clicked(refresh)
-
-def submit_nodes(count):
-    global node_count
-    node_count = int(count)
-
-axbox = plt.axes((0.45, 0.05, 0.07, 0.07))
-text_box = TextBox(axbox, "nodes: ", initial=str(node_count))
-text_box.on_submit(submit_nodes)
+    def refresh(event):
+        ax.clear()
+        graph = generate_random_graph(node_count)
+        pos = nx.spring_layout(graph)
+        plot_graph(graph, ax, pos)
+        baruah(graph, ax, pos)
+        fig.canvas.draw()
 
 
-axslider = plt.axes((0.7, 0.05, 0.2, 0.07))
-slider = Slider(axslider, "time", 0, 100, 0, valstep=1)
+    axbutton = plt.axes((0.2, 0.05, 0.1, 0.07))
+    button = Button(axbutton, "new")
+    button.on_clicked(refresh)
 
-def onclick(event):
-    if 120 < event.y:
-        return
+    def submit_nodes(count):
+        global node_count
+        node_count = int(count)
 
-fig.canvas.mpl_connect('button_press_event', onclick)
+    axbox = plt.axes((0.45, 0.05, 0.07, 0.07))
+    text_box = TextBox(axbox, "nodes: ", initial=str(node_count))
+    text_box.on_submit(submit_nodes)
 
-plt.show()
+
+    axslider = plt.axes((0.7, 0.05, 0.2, 0.07))
+    slider = Slider(axslider, "time", 0, 100, 0, valstep=1)
+
+    def onclick(event):
+        if 120 < event.y:
+            return
+
+    fig.canvas.mpl_connect('button_press_event', onclick)
+
+    plt.show()
 

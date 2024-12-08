4cf9693fbadf813c10bf4b24bc9d2d87763aa34d
yayaduckd <114066508+yayaduckd@users.noreply.github.com>
2024-12-08 19:17:51 +0100

    start raindrop

--- btypes.py
+++ btypes.py
@@ -24,14 +24,14 @@ class Edge:
 
     def get_other_side(self, node):
         return self.to_node if self.from_node == node else self.from_node
-    
+
     # equals
     def __eq__(self, other):
         return self.from_node == other.from_node and self.to_node == other.to_node and self.expected_delay == other.expected_delay and self.worst_case_delay == other.worst_case_delay
-    
+
     def __str__(self):
         return f"Edge {self.from_node} -({self.expected_delay}, {self.worst_case_delay})-> {self.to_node}"
-    
+
     def __hash__(self):
         return hash((self.from_node, self.to_node, self.expected_delay, self.worst_case_delay))
 
@@ -44,19 +44,19 @@ class Entry:
 
     # equals
     def __eq__(self, other):
-        return hash(self) == hash(other) 
-    
+        return hash(self) == hash(other)
+
     # less than
     def __lt__(self, other):
         return hash(self) < hash(other)
-    
+
     # hash
     def __hash__(self):
         return hash((self.parent, self.max_time, self.expected_time))
-    
+
     def __str__(self):
         return f"Entry: {self.parent} {self.max_time} {self.expected_time}"
-    
+
     def __repr__(self):
         return self.__str__()
 
@@ -83,10 +83,10 @@ class Graph:
 
     def modify_edge(self, u:Node, v:Node, data: Tuple[int, int]):
         self.data[u][v] = data
-    
+
     def nodes(self: Graph) -> List[Node]:
         return list(self.data.keys())
-    
+
     def edges(self: Graph) -> Set[Edge]:
         edges: Set[Edge] = set()
 
@@ -102,29 +102,29 @@ class Graph:
             neighbors.append(neighbor)
 
         return neighbors
-    
+
     def neighbor_of(self: Graph, node: Node) -> List[Node]:
         neighborof = []
         for (n, edges) in self.data.items():
             if node in edges.keys():
                 neighborof.append(n)
         return neighborof
-        
-    
+
+
     def outgoing_edges(self: Graph, node: Node) -> List[Edge]:
         edges = []
         for (node, neighbors) in self.data.items():
             for (neighbor, weights) in neighbors.items():
                 edges.append(Edge(node, neighbor, *weights))
         return edges
-    
+
     def incoming_edges(self: Graph, node: Node) -> List[Edge]:
         edges = []
         for (n, neighbors) in self.data.items():
             if node in neighbors.keys():
                 edges.append(Edge(n, node, *neighbors[node]))
         return edges
-    
+
     def __str__(self):
         result = ""
         for (node, neighbors) in self.data.items():
@@ -134,8 +134,8 @@ class Graph:
             for (neighbor, weights) in neighbors.items():
                 result += f"    -({weights[0]}, {weights[1]})-> {neighbor}\n"
 
-        return result 
-    
+        return result
+
 def test():
     G = Graph({
         1: {2: (4, 10), 4: (15, 25)},
@@ -158,9 +158,12 @@ class TemporalGraph:
 
     def at_time(self, t: int) -> Graph:
         return self.time_to_graph[t]
-    
+
+    def num_nodes(self):
+        return len(self.time_to_graph[0].nodes())
+
     def __str__(self):
         string = ""
         for i, graph in enumerate(self.time_to_graph):
-            string += (f"Time {i}: {graph}\n")
+            string += (f"Time {i}:\n{graph}\n")
         return string
---
+++ raindrop.py
@@ -0,0 +1,202 @@
+from audioop import max
+from turtledemo.chaos import f
+from copy import deepcopy
+import random
+from baruah_modified import original_baruah
+from btypes import Graph, Entry, Tables, Table, TemporalGraph, Node, Edge
+from typing import List
+from dataclasses import dataclass
+from multiprocessing import Process, Queue
+from time import sleep
+
+NodeLabel = int | str
+
+
+@dataclass
+class NeighborUpdate:
+    deadline: int
+    difference: int
+
+
+@dataclass
+class NodeData:
+    parent_queues: List[Queue]  # THESE ARE NODES WITH EDGES TO THIS NODE
+    edges: List[Edge]  # THESE ARE EDGES FROM THIS NODE
+    table: Table
+
+
+def reduce_table(table: Table):
+    new_table = deepcopy(table)
+    for entry in table.entries:
+        for entry2 in table.entries:
+            if entry == entry2:
+                continue
+            if (
+                entry.expected_time <= entry2.expected_time
+                and entry.max_time <= entry2.max_time
+            ):
+                if entry2 in new_table.entries:
+                    new_table.entries.remove(entry2)
+    table.entries = new_table.entries
+
+
+def process_edge_change(node: Node, edge: Edge, node_data: NodeData):
+    old_edge = None
+    for e in node_data.edges:
+        if e.from_node == edge.from_node and e.to_node == edge.to_node:
+            old_edge = e
+            break
+    if old_edge is None:
+        raise ValueError("Edge not found in node data")
+    # iterate over all entries in the table
+    new_entries = []
+    removed_entries = []
+    for entry in node_data.table.entries:
+
+        if entry.parent == edge.to_node:
+
+            # already notify parents
+            # for parent_queue in node_data.parent_queues:
+            #     parent_queue.put(
+            #         NeighborUpdate(
+            #             entry.max_time, edge.expected_delay - old_edge.expected_delay
+            #         )
+            #     )
+
+            removed_entries.append(entry)
+            # update the entry
+            new_entry = Entry(
+                entry.max_time,
+                edge.to_node,
+                entry.expected_time + (edge.expected_delay - old_edge.expected_delay),
+            )
+            new_entries.append(new_entry)
+    print(f"Node {node} old table {node_data.table}")
+    for entry in removed_entries:
+        node_data.table.entries.remove(entry)
+    for entry in new_entries:
+        node_data.table.entries.append(entry)
+    reduce_table(node_data.table)
+    print(f"Node {node} new table {node_data.table}")
+
+    return
+
+
+def process_neighbor_update(node: Node, update: NeighborUpdate, node_data: NodeData):
+    # find entry with smallest deadline >= update.deadline
+    for entry in node_data.table.entries:
+        if entry.parent in node_data.parent_queues:
+            if entry.max_time >= update.deadline:
+                # update the entry
+                new_entry = Entry(
+                    entry.max_time,
+                    entry.parent,
+                    entry.expected_time + update.difference,
+                )
+                node_data.table.entries.append(new_entry)
+    reduce_table(node_data.table)
+    return
+
+
+def node_loop(node: Node, node_data: NodeData, queue: Queue) -> None:
+    while True:
+        received: Edge | NeighborUpdate | None = queue.get()
+        if isinstance(received, NeighborUpdate):
+            print(f"Node {node} received neighbor update {received}")
+            # process neighbor update
+            process_neighbor_update(node, received, node_data)
+            continue
+        elif isinstance(received, Edge):
+            process_edge_change(node, received, node_data)
+            continue
+        elif received is None:
+            print(f"Node {node} exiting")
+            break
+    return
+
+
+def randomly_modify_graph(graph: Graph) -> Graph:
+    new_graph = deepcopy(graph)
+    # pick one edge randomly_modify_graph
+    edge = random.choice(list(new_graph.edges()))
+    # modify the edge
+    # modify expected delay only
+    new_graph.modify_edge(
+        edge.from_node,
+        edge.to_node,
+        (random.randint(1, edge.worst_case_delay), edge.worst_case_delay),
+    )
+    return new_graph
+
+
+def main():
+    baruah_paper_graph = Graph(
+        {
+            1: {2: (4, 10), 4: (15, 25)},
+            2: {3: (4, 10), 4: (12, 15)},
+            3: {4: (4, 10)},
+            4: {},
+        }
+    )
+
+    initial_tables = original_baruah(baruah_paper_graph, 4, False)
+
+    graph_list: List[Graph] = []
+    g2 = deepcopy(baruah_paper_graph)
+    g2.modify_edge(2, 3, (9, 10))
+    graph_list.append(baruah_paper_graph)
+    graph_list.append(g2)
+
+    random.seed(105)
+    for i in range(10):
+        graph_list.append(randomly_modify_graph(graph_list[-1]))
+
+    temporal_graph = TemporalGraph(graph_list)
+    print(temporal_graph)
+    print(initial_tables)
+
+
+    # start loop for all nodes
+    # use multiprocessing to parallelize the loop
+    queues = {}
+    for i in temporal_graph.at_time(0).nodes():
+        queues[i] = Queue()
+    processes = {}
+    for i in temporal_graph.at_time(0).nodes():
+        node = i
+        adjecent_queues = [
+            queues[j] for j in temporal_graph.at_time(0).neighbor_of(node)
+        ]
+        data = NodeData(
+            adjecent_queues,
+            temporal_graph.at_time(0).outgoing_edges(node),
+            initial_tables[node],
+        )
+        queues[node] = Queue()
+        p = Process(target=node_loop, args=(node, data, queues[node]))
+        processes[node] = p
+        p.start()
+
+    time = 0
+    current_graph = temporal_graph.at_time(0)
+    for i in range(len(temporal_graph) - 1):
+        time += 1
+        current_graph = temporal_graph.at_time(time)
+        changed_edges = current_graph.edges() - temporal_graph.at_time(time - 1).edges()
+        for edge in changed_edges:
+            # only 1 edge is changed for now
+            queues[edge.from_node].put(edge)
+        sleep(1)
+        pass
+
+    # join all processes
+    for i in temporal_graph.at_time(0).nodes():
+        queues[i].put(None)
+        processes[i].join()
+
+    print(initial_tables)
+    return
+
+
+if __name__ == "__main__":
+    main()
